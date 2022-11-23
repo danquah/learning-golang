de6990bfe7b21d16ac8fe3028c2792cbe3c8560c
Mads H. Danquah <mads@danquah.dk>
2022-11-23 09:01:28 +0000

    Working on section 4

---
+++ exercises/04/language/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Use the short declaration operator
+	x := 42
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
+	y := 100 + 70
+	fmt.Println(y)
+	z := "Muggie, Wuggi"
+	fmt.Println(z)
+}
